0337f9000d7a1503a16abc9112d6001db209071d
mscarce <oluwa14tobi@gmail.com>
2024-04-05 12:08:34 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="Moscarce"
+
+ENTRYPOINT ["top", "-b"]
---
+++ src/main/java/com/example/dockerlearn/DockerLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerLearnApplication.class, args);
+    }
+
+}
